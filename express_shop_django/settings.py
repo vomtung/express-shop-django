909c5edc521af40abaeea6d191754c32de6c9352
TonyVo <tonyvohcmut@gmail.com>
2025-06-14 20:30:12 +0700

    id-3: mainhome

--- express_shop_django/settings.py
+++ express_shop_django/settings.py
@@ -75,10 +75,18 @@ WSGI_APPLICATION = 'express_shop_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'express_shop_django',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',  # hoặc IP database server
+        'PORT': '3306',        # cổng mặc định của MySQL
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
+
 
 
 # Password validation
