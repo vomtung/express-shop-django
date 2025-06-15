1fd3ea0ac6eda56449462675c0eb9f9c5e156dab
TonyVo <tonyvohcmut@gmail.com>
2025-06-15 12:40:47 +0700

    id-3: main home app

--- main/views.py
+++ main/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from main.models import ApplicationSetting
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def home(request):
-    return render(request, 'index.html')
+    try:
+        app_title = ApplicationSetting.objects.get(config_key='APP_TITLE')
+    except ApplicationSetting.DoesNotExist:
+        app_title = None
+    return render(request, 'index.html', {'app_title': app_title})
