efe0937580589565166563ff9bc85b304d99bb3c
TonyVo <tonyvohcmut@gmail.com>
2025-06-15 11:55:54 +0700

    id-3: main home

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class ApplicationSetting(models.Model):
+    config_key = models.CharField(max_length=100)
+    config_value = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        db_table = 'application_settings'  # Chính xác tên bảng trong MySQL
+
+    def __str__(self):
+        return f"{self.config_key}: {self.config_value}"
